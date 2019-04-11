396c1bf6b28883a4252f4b85ec726971a9ee7e84
tomcatsf1 <tomc@Toms-MacBook.local>
2019-04-11 12:53:18 -1000

    waitList

--- exercises.js
+++ exercises.js
@@ -8,6 +8,15 @@ Write a for-loop that will iterate through 20 numbers (starting at 1 and ending 
 .
 "Now serving 20."
 */
+// for (var i = 0; i<10; i ++){
+
+// }
+
+var waitList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+for (var i = 0; i<waitList.length; i++){
+   console.log("Now serving " + waitList[i]);  
+};
 
 
 /* 2) Pop Charts
